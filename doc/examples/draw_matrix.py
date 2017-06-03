066b9bd650da46b5d2626fc574fb1ce42dcdb383
gronki <gronki@gmail.com>
2017-06-04 01:38:50 +0200

    przykład jak narysować macierz

---
+++ doc/examples/draw_matrix.py
@@ -0,0 +1,78 @@
+
+# coding: utf-8
+
+# In[43]:
+
+from diskvert import libdv
+import numpy as np
+
+libdv.init_disk(10,0.01,10)
+libdv.init_ss73(0.02)
+libdv.eval_globals()
+
+nz = 1000
+z = np.ndarray(nz, np.float64)
+y = np.ndarray((nz,4), np.float64)
+dy = np.ndarray((nz,4), np.float64)
+a = np.ndarray((nz,8), np.float64)
+
+libdv.run_ss73(z,nz,y,dy,a)
+
+
+# In[44]:
+
+from ctypes import CDLL, POINTER, c_double, c_float, c_int
+from numpy.ctypeslib import ndpointer
+
+
+# In[45]:
+
+libdv.generate_coefficients.restype = None
+libdv.generate_coefficients.argtypes = [
+    ndpointer(c_double,1),
+    c_int,
+    ndpointer(c_double,1),
+    c_int,
+    ndpointer(c_double,2),
+    ndpointer(c_double,1),
+]
+
+
+# In[46]:
+
+nx = 30
+ny = 3
+x = np.linspace(0, z.max(), nx)
+Y = np.ndarray(nx*ny)
+Y[0::ny] = np.interp(x[::-1],z[::-1],a[:,2])
+Y[1::ny] = np.interp(x[::-1],z[::-1],a[:,1])
+Y[2::ny] = np.interp(x[::-1],z[::-1],y[:,2])
+
+
+# In[47]:
+
+A = np.zeros((nx*ny))
+M = np.zeros((nx*ny,nx*ny))
+
+
+# In[48]:
+
+#libdv.init_ss73(0.04)
+#libdv.eval_globals()
+libdv.generate_coefficients(x,nx,Y,ny,M,A)
+
+
+# In[49]:
+
+import matplotlib.pyplot as plt
+plt.pcolor(np.arcsinh(M * 1e8), vmin=-50, vmax=50, cmap='seismic')
+plt.ylabel('Y')
+plt.xlabel('eq')
+plt.colorbar()
+plt.show()
+
+
+# In[ ]:
+
+
+
