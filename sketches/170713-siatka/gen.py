075ae3156f3b71913b3cb2ff308f732ee9e3b0fe
gronki <gronki@gmail.com>
2017-07-30 22:59:24 +0200

    generowanie siatki

--- sketches/170713-siatka/gen.py
+++ sketches/170713-siatka/gen.py
@@ -10,18 +10,20 @@ zeta {zeta:.5e}
 # beta = {beta:.5e}
 """
 
-nmdot = 10
-nbeta = 7
-nrad = 18
+
+nmdot = 12
+nbeta = 12
+nrad = 24
+alpha = 0.01
 
 izip = lambda x: zip(range(len(x)),x)
-alpha = 0.01
+
 for imdot,mdot in izip(np.logspace(-3,-1,nmdot)):
     betamax = 2 / alpha - 1
     for ibeta,beta in izip(np.logspace(0,np.log10(betamax),nbeta)):
         zeta = alpha * (beta + 1) / 2
-        for irad,rad in izip(np.logspace(np.log10(5),np.log10(50),nrad)):
-            fn = 'M{:02d}R{:02d}B{:02d}'.format(imdot,irad,ibeta)
+        for irad,rad in izip(np.logspace(np.log10(5),np.log10(100),nrad)):
+            fn = 'M{:02d}R{:02d}B{:02d}'.format(imdot+1,irad+1,ibeta+1)
             with open('{}.par'.format(fn),'w') as f:
                 f.write(F.format(
                     fn = fn,
--- sketches/170713-siatka/job.sh
+++ sketches/170713-siatka/job.sh
@@ -1,5 +1,10 @@
+J=${1%.par}
+
 DVFLAGS="-no-bf"
 
-cat $1.par | dv-mag-rx $DVFLAGS          -o "${1}D"
-cat $1.par | dv-mag-rx $DVFLAGS -compton -o "${1}W"
-cat $1.par | dv-mag-rx $DVFLAGS -corona  -o "${1}C"
+cat $J.par | dv-mag-rx $DVFLAGS          -o "${J}D"
+cat $J.par | dv-mag-rx $DVFLAGS -compton -o "${J}W"
+cat $J.par | dv-mag-rx $DVFLAGS -corona  -o "${J}C"
+
+python plot.py $J
+
