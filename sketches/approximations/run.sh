b241dfc6a160887e8988c8da5c54d01173755a48
gronki <gronki@gmail.com>
2017-08-06 23:15:58 +0200

    parentheses!

--- sketches/approximations/run.sh
+++ sketches/approximations/run.sh
@@ -1,14 +1,13 @@
-opacities= -no-bf
+dvflags='-no-bf'
 
-cat input.par | dv-mag-rx ${opacities} -corona    -o MCFX &
-cat input.par | dv-mag-rx ${opacities} -compton   -o MWFX &
-cat input.par | dv-mag-rx ${opacities}            -o MDFX &
-
-cat input.par | dv-mag    ${opacities} -compton   -o MWFK &
-cat input.par | dv-mag    ${opacities} -compton2  -o MQFK &
-cat input.par | dv-mag    ${opacities} -corona    -o MCFK &
-
-wait
+#cat - | parallel <<EOF
+cat input.par | dv-mag-rx ${dvflags} -corona    -o MCFX
+cat input.par | dv-mag-rx ${dvflags} -compton   -o MWFX
+cat input.par | dv-mag-rx ${dvflags}            -o MDFX
+cat input.par | dv-mag    ${dvflags} -compton   -o MWFK
+cat input.par | dv-mag    ${dvflags} -compton2  -o MQFK
+cat input.par | dv-mag    ${dvflags} -corona    -o MCFK
+#EOF
 
 python plot.py &
 
