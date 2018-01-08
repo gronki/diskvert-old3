c1c2958972ac1fe815d54e91ccb1a22dc6b31378
gronki <gronki@gmail.com>
2018-01-08 18:43:07 +0100

    zapisywanie listy jobów w GRID MRB

--- sketches/grid-mrb/gen.py
+++ sketches/grid-mrb/gen.py
@@ -13,6 +13,8 @@ rads =  logspace(log10(3.1), log10(200), 48)
 
 izip = lambda x: zip(range(len(x)),x)
 
+fj = open('jobs.lst','w')
+
 for imdot,mdot in izip(mdots):
     for ibeta,beta in izip(betas):
         zeta = alpha * (beta + 1) / 2
@@ -26,5 +28,7 @@ for imdot,mdot in izip(mdots):
                 f.write( fmt.format('alpha',  alpha ))
                 f.write( fmt.format('zeta',   zeta  ))
                 f.write(fmtc.format('beta',   beta  ))
+            fj.write(fn + '\n')
 
+fj.close()
 print "generated {} files".format(len(mdots)*len(betas)*len(rads))
