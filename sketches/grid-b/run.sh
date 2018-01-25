f5086e4ed9714a272816079acb561325b9246a7f
gronki <gronki@gmail.com>
2018-01-25 01:32:06 +0100

    usunięcie starych gridów

--- sketches/grid-b/run.sh
+++
@@ -1,1 +0,0 @@
-parallel bash job.sh {.} ::: *.par
--- sketches/grid-mrb/job.sh
+++
@@ -1,8 +0,0 @@
-DVFLAGS="-no-bf -top 120"
-
-cat "par/${1}" | dv-mag-rx $DVFLAGS          -o "data/${1}D"
-cat "par/${1}" | dv-mag-rx $DVFLAGS -compton -o "data/${1}W"
-cat "par/${1}" | dv-mag-rx $DVFLAGS -corona  -o "data/${1}C"
-
-python plot.py "${1}"
-diskvert-cooling2D "data/${1}C.dat" -o "cool/${1}C.png"
