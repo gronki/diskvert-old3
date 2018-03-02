a49885f2e616aa43e11c286bf9574297d420db52
gronki <gronki@gmail.com>
2018-03-02 23:19:24 +0100

    test

--- sketches/test/run.sh
+++ sketches/test/run.sh
@@ -1,11 +1,26 @@
-dvflags=""
+dvflags="-linear"
 
+tee input.par <<EOF
+mbh    10
+mdot   0.05
+radius 6
+alpha  0.1
+zeta   0.5
+nu     0.0
+EOF
+
+parallel <<EOF
 cat input.par | dv-mag-rx $dvflags -o diskd
-cat input.par | dv-mag-rx $dvflags -o diskw -compton
-cat input.par | dv-mag-rx $dvflags -o diskp -compton -post-corona
-cat input.par | dv-mag-rx $dvflags -o diskq -post-corona
-cat input.par | dv-mag-rx $dvflags -o diskc -corona 
+cat input.par | dv-mag-rx $dvflags -o diskds  -quench 
+cat input.par | dv-mag-rx $dvflags -o diskw   -compton
+cat input.par | dv-mag-rx $dvflags -o diskdp  -post-corona
+cat input.par | dv-mag-rx $dvflags -o diskwp  -compton -post-corona
+cat input.par | dv-mag-rx $dvflags -o diskwps -compton -post-corona -quench
+cat input.par | dv-mag-rx $dvflags -o diskc   -corona 
+cat input.par | dv-mag-rx $dvflags -o diskcr  -corona -relcompt
+cat input.par | dv-mag-rx $dvflags -o diskcs  -corona -quench 
+EOF
 
 parallel python plotone.py ::: disk*.dat
-parallel diskvert-cooling2D ::: disk*.dat
+# parallel diskvert-cooling2D ::: disk*.dat
 
